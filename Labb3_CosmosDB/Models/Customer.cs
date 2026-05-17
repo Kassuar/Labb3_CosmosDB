f07095ebdcfc30ce0ad41e4371c805df75598ae2
Kassu <Kassu@DESKTOP-83JCDOV>
2026-05-17 16:04:28 +0200

    Added Git changes to workflow

--- Labb3_CosmosDB/Models/Customer.cs
+++ Labb3_CosmosDB/Models/Customer.cs
@@ -4,15 +4,15 @@
     {
       public string id { get; set; }
 
-      public string Name { get; set; }
+      public string Name { get; set; } = string.Empty;
 
-      public string Title { get; set; }
+      public string Title { get; set; } = string.Empty;
 
-      public string Telephone { get; set; }
+      public string Telephone { get; set; } = string.Empty;
 
-      public string Email { get; set; }
+      public string Email { get; set; } = string.Empty;
 
-      public string Address { get; set; }
+      public string Address { get; set; } = string.Empty;
 
       public Seller Seller { get; set; }  
         
