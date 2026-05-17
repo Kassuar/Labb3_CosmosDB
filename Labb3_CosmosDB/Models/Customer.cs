09a864955ddf2e8aa85ad9a730029375a2ff414f
Kassu <Kassu@DESKTOP-83JCDOV>
2026-05-17 15:23:43 +0200

    Add project files.

---
+++ Labb3_CosmosDB/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace Labb3_CosmosDB.Models
+{
+    public class Customer
+    {
+      public string id { get; set; }
+
+      public string Name { get; set; }
+
+      public string Title { get; set; }
+
+      public string Telephone { get; set; }
+
+      public string Email { get; set; }
+
+      public string Address { get; set; }
+
+      public Seller Seller { get; set; }  
+        
+    }
+}
